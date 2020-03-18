308aa740ee1346aa16e08ea03726a855a48ed2b6
ymad47 <ymadbg@gmail.com>
2020-03-18 16:05:51 +0100

    Help me

--- backend/post_Analitics.php
+++ backend/post_Analitics.php
@@ -1,8 +1,10 @@
 <?php
 	function addlogevent($event){
+		
+		$content = $_POST['content'];
+		$atcion = $_POST['action'];
+		$event =$action.$event."\n";
+		$event=$content.$event."\n";
 		file_put_contents("fichier.log", $event."\n", FILE_APPEND);
-		$content = $_POST[''];
-		$atcion = $_POST[''];
-
 	}
 ?>
