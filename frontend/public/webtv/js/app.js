35767b9f36e3307c58fe8a6b0e2510c2400630ed
student <hi.soulier@gmail.com>
2020-03-18 12:20:43 +0100

    First commit nouvelle branche

--- frontend/public/webtv/js/app.js
+++ frontend/public/webtv/js/app.js
@@ -1,29 +1,47 @@
-function loadVideo(strurl) {
-    var player = document.getElementById("videoPlayer");
-    //dashjs.MediaPlayer().reset();
-    var dashpl = dashjs.MediaPlayer().create();
-    dashpl.initialize(player, strurl, true);
-}
-
-let divrep = document.querySelector('.section.linkVOD .container ul')
-
+// Event chargement liens dynamiques VODs
 window.addEventListener('load', () => {
     requete()
-})
+}, false)
 
 
+// Création des liens des VODs
+const linkCreator = (resp, parent) => {
+    resp.forEach(el => {
+        parent.insertAdjacentHTML('afterbegin', `<li><a href="${el.videoUrl}" class="has-text-primary">${el.videoId}</a></li>`)
+    })
+    parent.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault()
+            console.log(e.target.href)
+            loadManifest()
+        })
+    })
+}
+
+// Requete XHR
 function requete() {
-    xhr = new XMLHttpRequest();
+    let linkMpd = document.querySelector('.section.linkVOD .container ul')
+    xhr = new XMLHttpRequest()
     xhr.addEventListener('readystatechange', function () {
         if (xhr.readyState === XMLHttpRequest.DONE && xhr.readyState === 4) {
-            let response = JSON.parse(xhr.responseText);
-            response.forEach(el => {
-                divrep.innerHTML += `<li><a href="${el.videoUrl}" class="has-text-primary">${el.videoId}</a></li>`
-            })
+            let response = JSON.parse(xhr.responseText)
+            linkCreator(response, linkMpd)
         }
     })
-    
-    xhr.open('GET', 'http://localhost:8080/backend/get_content_list.php');
+    xhr.open('GET', 'http://localhost:8080/backend/get_content_list.php')
     xhr.send(null)
+}
 
+// Chargement du manifeste
+function loadManifest(mpd = "http://localhost:8080//BentoDash/test_1/stream.mpd") {
+    let video,
+        player,
+        url = mpd;
+
+    video = document.querySelector(".videoDashJs");
+    // video.innerHTML = ''
+    // dashjs.MediaPlayer().reset();
+    // player = dashjs.MediaPlayer().create();
+    // player.initialize(video, url, true);
+    video.src = mpd;
 }
