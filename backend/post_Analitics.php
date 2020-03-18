520b1c1b89166dd0afd1f2faa52c4334e7b88137
ymad47 <ymadbg@gmail.com>
2020-03-18 16:27:48 +0100

    Inchallah

--- backend/post_Analitics.php
+++ backend/post_Analitics.php
@@ -1,10 +1,11 @@
 <?php
 	function addlogevent($event){
-		
-		$content = $_POST['content'];
-		$atcion = $_POST['action'];
-		$event =$action.$event."\n";
-		$event=$content.$event."\n";
-		file_put_contents("fichier.log", $event."\n", FILE_APPEND);
+
+		$content=$_POST['content'];
+		$action=$_POST['action];
+
+		file_put_contents("fichier.log", $content.",".action.",".$event"\n", FILE_APPEND)
+;
+		$event=$content.$action.$event."\n";
 	}
 ?>
