1ba67ebd7b21b81ec7623c34de5a5b511f30b75c
g-maulino <gerard.maulino@castlabs.com>
2020-03-18 11:57:29 +0100

    Fix typo

--- backend/packageur/Bento-DASH.sh
+++ backend/packageur/Bento-DASH.sh
@@ -2,11 +2,12 @@
 #   bento-dash <input file> <output dir>
 #
 BentoDir='/usr/local/Bento4'
-cmsdata='../cms/data'
+cmsdata='../cms.data'
 mediaserveur='localhost:8080'
 $BentoDir/bin/mp4fragment --track video $1 frag_video.mp4
 $BentoDir/bin/mp4fragment --track audio $1 frag_audio.mp4
 $BentoDir/bin/mp4dash --profiles='urn:mpeg:dash:profile:isoff-on-demand:2011' -o output/$2/ frag_video.mp4 frag_audio.mp4
-echo $2,http://$mediaserveur/content/$2/stream.mpd,1,http://$mediaserveur/content/$2/thumb.jpg,Description_$2 >> $cmsdata
+ffmpeg -i $1 -vframes 1 -an -filter:v scale="-1:100" -ss 20 output/$2/thumb.jpg
+echo $2,http://$mediaserveur/BentoDash/$2/stream.mpd,1,http://$mediaserveur/BentoDash/$2/thumb.jpg,Description_$2 >> $cmsdata
 
 
