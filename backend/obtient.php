268dd92b4163e69a6536081dd0c88f407d03c4cb
ymad47 <ymadbg@gmail.com>
2020-03-23 11:42:00 +0100

    Zebi

---
+++ backend/obtient.php
@@ -0,0 +1,72 @@
+<?php
+
+function getBrowser() { 
+  $u_agent = $_SERVER['HTTP_USER_AGENT'];
+  $bname = 'Unknown';
+  $version= "";
+
+
+  // Noms des navigateurs
+  if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent)){
+    $bname = 'Internet Explorer';
+    $ub = "MSIE";
+  }elseif(preg_match('/Firefox/i',$u_agent)){
+    $bname = 'Mozilla Firefox';
+    $ub = "Firefox";
+  }elseif(preg_match('/OPR/i',$u_agent)){
+    $bname = 'Opera';
+    $ub = "Opera";
+  }elseif(preg_match('/Chrome/i',$u_agent) && !preg_match('/Edge/i',$u_agent)){
+    $bname = 'Google Chrome';
+    $ub = "Chrome";
+  }elseif(preg_match('/Safari/i',$u_agent) && !preg_match('/Edge/i',$u_agent)){
+    $bname = 'Apple Safari';
+    $ub = "Safari";
+  }elseif(preg_match('/Netscape/i',$u_agent)){
+    $bname = 'Netscape';
+    $ub = "Netscape";
+  }elseif(preg_match('/Edge/i',$u_agent)){
+    $bname = 'Edge';
+    $ub = "Edge";
+  }elseif(preg_match('/Trident/i',$u_agent)){
+    $bname = 'Internet Explorer';
+    $ub = "MSIE";
+  }
+
+  // choix des versions wola
+  $known = array('Version', $ub, 'other');
+  $pattern = '#(?<browser>' . join('|', $known) .
+')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
+  if (!preg_match_all($pattern, $u_agent, $matches)) {
+    
+  }
+  
+  $i = count($matches['browser']);
+  if ($i != 1) {
+ 
+    if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
+        $version= $matches['version'][0];
+    }else {
+        $version= $matches['version'][1];
+    }
+  }else {
+    $version= $matches['version'][0];
+  }
+
+ 
+  if ($version==null || $version=="") {$version="?";}
+
+  return array(
+    'userAgent' => $u_agent,
+    'name'      => $bname,
+    'version'   => $version,
+     'pattern'    => $pattern
+  );
+} 
+
+    // nl'éxecution'
+    $ua=getBrowser();
+    $zebi= "Ton navigateur est  " . $ua['name']. " wola  ";
+    print_r($zebi);
+?>
+
