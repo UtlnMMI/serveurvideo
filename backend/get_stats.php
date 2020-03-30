d0381cdf151b901c7d47bb1a9e1fb74c989d6a8f
g-maulino <gerard.maulino@castlabs.com>
2020-03-30 16:20:53 +0200

    Fix git_stat

--- backend/get_stats.php
+++ backend/get_stats.php
@@ -1,7 +1,13 @@
 <?php
-  $fn = fopen("fichier.log","r");
-  $labels = array();
-  $data = array();  
+  $fn = fopen("data.log","r");
+  $lignes = array();
+  $ratio = array();
+  
+  $brows_pie = false;
+  
+  if(isset($_GET["browsers"]))
+    $brows_pie = true;
+    
   $result = fgets($fn);
 
   while(! feof($fn) )  {
@@ -10,42 +16,73 @@
 	$action = strtok(',');
 	$browser = strtok(',');
 
-    $el = array_search($videoID, $labels);
-
-    if( $el != FALSE) {
-		$data[$el] = $data[$el]+1;
-		}
-    else
-    {
-        array_push($labels,$videoID);
-        array_push($data,1);
-    }
+    array_push($lignes,$videoID);
+    array_push($ratio,$browser);
+    
     $result = fgets($fn);
   }
 
   fclose($fn);
   
-  // display
-  printf("[\n");
-  if(count($labels)>0)
-    printf("{\"label\": [");
-    
-  for ($i=0; $i<count($labels); $i++) {
-    printf("\"%s\"",$labels[$i]);
-    if($i!=count($labels)-1)
-        printf(",");
+  if( $brows_pie === true ) 
+  {
+      $res = array_count_values($ratio);
+      // display   
+       
+      $label = array_keys($res);
+      $data = array_values($res);
+       
+      printf("[\n");
+      if(count($label)>0)
+        printf("{\"label\": [");
+        
+      for ($i=0; $i<count($label); $i++) {
+        printf("\"%s\"",$label[$i]);
+        if($i!=count($label)-1)
+            printf(",");
+      }
+      if(count($label)>0)
+        printf("],\"data\": [");
+        
+      for ($i=0; $i<count($data); $i++) {
+        printf("\"%s\"",$data[$i]);
+        if($i!=count($data)-1)
+            printf(",");
+      }
+      if(count($data)>0)
+        printf("]}");
+      
+      printf("]\n");
   }
-  if(count($labels)>0)
-    printf("],\"data\": [");
-    
-  for ($i=0; $i<count($data); $i++) {
-    printf("\"%s\"",$data[$i]);
-    if($i!=count($data)-1)
-        printf(",");
+  else
+  {
+      $res = array_count_values($lignes);
+      // display   
+
+      $label = array_keys($res);
+      $data = array_values($res);
+       
+      printf("[\n");
+      if(count($label)>0)
+        printf("{\"label\": [");
+        
+      for ($i=0; $i<count($label); $i++) {
+        printf("\"%s\"",$label[$i]);
+        if($i!=count($label)-1)
+            printf(",");
+      }
+      if(count($label)>0)
+        printf("],\"data\": [");
+        
+      for ($i=0; $i<count($data); $i++) {
+        printf("\"%s\"",$data[$i]);
+        if($i!=count($data)-1)
+            printf(",");
+      }
+      if(count($data)>0)
+        printf("]}");
+      
+      printf("]\n");
   }
-  if(count($data)>0)
-    printf("}");
-  
-  printf("]\n");
 ?>
 
